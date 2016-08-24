d220b44869f49db14d96ce87399761f865eca97e
Zac Braddy <zacharybraddy@gmail.com>
2016-08-24 22:30:26 +0100

    Intial content commit

---
+++ script/codeQuest.js
@@ -0,0 +1,27 @@
+
+var CodeQuest = React.createClass({
+  displayName: 'Hello',
+  getInitialState: function getInitialState() {
+    return { name: 'Friend' };
+  },
+  render: function() {
+    return React.createElement('div', null,
+      React.createElement('input', {type: 'text', onChange: this.handleChange}),
+      React.createElement('div', {className: 'scroll'},
+        React.createElement('div', null, 'Hello ', this.state.name),
+        React.createElement('div', null, 'I\'d like to invite you on a wonderous adventure towards the grok of ReactJS. Will you join me?'),
+        React.createElement('div', null, 'Zac Braddy'),
+        React.createElement('div', null, 'The Reactionary')
+      )
+    );
+  },
+  handleChange: function(e) {
+    console.log(e.target.value);
+    this.setState({name: e.target.value});
+  }
+});
+
+ReactDOM.render(
+  React.createElement(CodeQuest),
+  document.getElementById('the-kingdom')
+)
